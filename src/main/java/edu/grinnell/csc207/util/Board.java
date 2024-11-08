aab9bb516f93cd2dcfdf840582e6abf48521e918
root <root@Skeebo>
2024-11-07 18:58:20 -0600

    style edits

--- src/main/java/edu/grinnell/csc207/util/Board.java
+++ src/main/java/edu/grinnell/csc207/util/Board.java
@@ -2,24 +2,54 @@ package edu.grinnell.csc207.util;
 
 import java.io.PrintWriter;
 
+/**
+ * The class that controls the board and its interactions with the player/attackers.
+ */
 public class Board {
-  public final char def = ' ';
-  public final char playDef = 'O';
-  public final char attackDef = 'A';
   // +--------+------------------------------------------------------------
   // | Fields |
   // +--------+
+  /**
+   * the default character of our matrix.
+   */
+  public final char def = ' ';
+  /**
+   * The character that represents the player.
+   */
+  public final char playDef = 'O';
+  /**
+   * The character that represents the attackers.
+   */
+  public final char attackDef = 'A';
+  /**
+   * the width of the matrix/board.
+   */
   private final int width;
+  /**
+   * the height of the matrix/board.
+   */
   private final int height;
+  /**
+   * the matrix that we use to create the board.
+   */
   private static MatrixV0<Character> boardMatrix;
+  /**
+   * The player's actions and inputs.
+   */
   public Player player;
+  /**
+   * The attacker's actions.
+   */
   public Attacker attackers;
+  /**
+   * current level that the player is on, determines difficulty.
+   */
   public int level;
 
   // +--------------+------------------------------------------------------
   // | Constructors |
   // +--------------+
-  
+
   /**
    * Creates a Board with specified dimensions, player and attackers.
    * Start the player in the bottom center
@@ -42,12 +72,13 @@ public class Board {
 
   /**
    * Places attackers on the board for the player to clear.
+   *
    * @return true if the game ends, false if the game continues.
    */
   public boolean placeAttackers() {
     if (this.attackers.isGameOver()) {
       return true;
-    } else {
+    } /* if block */ else {
       this.attackers.placeAttackers(this.level);
       return false;
     } // if/else
@@ -61,35 +92,38 @@ public class Board {
    */
   public void clear(int row, int col) {
     Board.boardMatrix.set(row, col, this.def);
-  }
+  } //clear(row, col)
 
   /**
    * Inserts a row into the matrix field of the board.
-   * @param row row to insert into the matrix.
+   *
+   * @param row     row to insert into the matrix.
    * @param charArr array of characters to insert into the matrix.
    */
   public void insertRow(int row, Character[] charArr) {
     try {
       Board.boardMatrix.insertRow(row, charArr);
-    } catch (ArraySizeException e) {
+    } /* try block*/ catch (ArraySizeException e) {
       System.err.println("array size too big or small for matrix width");
-    }
-  }
+    } //catch(ArraySizeException)
+  } //insertRow(row, Character[])
 
   /**
    * Deletes a row from the board by accessing the matrix field.
+   *
    * @param row row to delete from the matrix.
    */
   public void deleteRow(int row) {
     Board.boardMatrix.deleteRow(row);
-  }
+  } //deleteRow(row)
 
   /**
-   * Increases the game level, increasing the amount of attackers that can spawn each time.
+   * Increases the game level, increasing the amount of attackers that can spawn
+   * each time.
    */
   public void increaseLevel() {
     this.level++;
-  }
+  } //increaseLevel()
 
   /**
    * Gets the value at the specified cell.
@@ -100,7 +134,7 @@ public class Board {
    */
   public Character get(int row, int col) {
     return Board.boardMatrix.get(row, col);
-  }
+  } //get(row, col)
 
   /**
    * Sets the value at the specified cell.
@@ -111,31 +145,29 @@ public class Board {
    */
   public void set(int row, int col, Character val) {
     Board.boardMatrix.set(row, col, val);
-  }
+  } //set(int, int, Character)
 
   /**
    * Gets the width of the board.
+   * @return width in int form.
    */
   public int getWidth() {
     return this.width;
-  }
+  } //getWidth()
 
   /**
    * Gets the height of the board.
+   * @return height in int form.
    */
   public int getHeight() {
     return this.height;
-  }
-  
+  } // getHeight()
+
   /**
    * Create and display the current board with a matrix and a caption.
-   *
-   * @param pen
-   *   The tool used to print output.
-   * @param caption
-   *   A caption for the matrix
-   * @param matrix
-   *   The matrix to print.
+   * @param pen The tool used to print output.
+   * @param caption A caption for the matrix
+   * @param score the current score of the player.
    */
   static void display(PrintWriter pen, String caption, int score) {
     pen.println("=".repeat(80));
